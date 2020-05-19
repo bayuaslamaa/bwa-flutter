a684962a293163b5e9525d0cc3fd5f0600a6fc5d
bayuaslamaa <bayuaslamaa@gmail.com>
2020-05-20 00:25:40 +0700

    from bwa done

---
+++ lib/custom-color.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color turqoise = Color(0XFF1abc9c);
---
+++ lib/font-style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'custom-color.dart';
+
+TextStyle mainHeader =
+    TextStyle(fontFamily: 'Pacifico', fontSize: 17, color: turqoise);
+
+TextStyle appBarText = TextStyle(fontFamily: 'Teko', color: Colors.black);
